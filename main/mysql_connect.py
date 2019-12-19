87c01fd9794dc00c8c4494a3dc12885d2f14b9e6
Maksim <kamax77@yandex.ru>
2019-12-19 22:36:56 +0500

    add graphql

--- main/mysql_connect.py
+++ main/mysql_connect.py
@@ -1,9 +1,21 @@
 connect = str('mysql+pymysql://root:@localhost/qarterdb?host=localhost?port=3306')
+# Необходимо создать установщик с вводом параметров подключения
+# username = str('root')
+# password = str(' ')
+# host = str('localhost')
+# dbname = str('qarterdb')
+# connect = str('mysql+pymysql://'+username+':'+password+
+# '@'+host+'/'+dbname)
 
-import sqlalchemy as sa
-from sqlalchemy import create_engine
- 
-engine = create_engine(connect)
+from sqlalchemy import *
+from sqlalchemy.orm import(scoped_session, sessionmaker, relationship, backref)
+from sqlalchemy.ext.declarative import declarative_base
+import graphene
+from graphene import relay
+from graphene_sqlalchemy import SQLAlchemyConnectionField, SQLAlchemyObjectType 
+
+engine = create_engine(connect, convert_unicode=True)
+db_session = scoped_session(sessionmaker(autocommit=False, autoflush=False, bind=engine))
 
 def table_exists(name):
     ret = engine.dialect.has_table(engine, name)
@@ -11,20 +23,45 @@ def table_exists(name):
     return ret
 
 def db_is_empty():
-    table_names = sa.inspect(engine).get_table_names()
+    table_names = inspect(engine).get_table_names()
     is_empty = table_names == []
 #    print('db is empty:{}'.format(is_empty))
     return is_empty
 
+# Автозаполнитель БД, переместить в установщик, когда будет готов
 if (db_is_empty()):
     engine.execute('create table users ('
-                   'id integer(11) not null,'
+                   'id_user integer(11) not null,'
                    'login varchar(30),'
                    'password varchar(20),'
                    'primary key (id));')     
 
 
-result = engine.execute('select * from users')
-for _r in result:
-    print(_r)
+#result = engine.execute('select * from users')
+#for _r in result:
+#    print(_r)
+
+base = declarative_base()
+base.query = db_session.query_property()
+
+class Users(base):
+    _tablename_ = 'users'
+    id_user = Column(Integer, primary_key=True)
+    login = Column(String)
+    password = Column(String)
+
+class UsersSchema(SQLAlchemyObjectType):
+    class Meta:
+        model = Users
+        interfaces = (relay.Node, )
+
+class UsersConnection(relay.Connection):
+    class Meta:
+        node = UsersSchema
+
+class Query(graphene.ObjectType):
+    node = relay.Node.Field()
+    all_users = SQLAlchemyConnectionField(UsersConnection)
+
+schema = graphene.Schema(query=Query)
 
